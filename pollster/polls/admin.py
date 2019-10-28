de3be1052943a7e9813e50657c314fc013d43fb4
PremePanther <shpadza@gmail.com>
2019-10-28 19:30:34 -0400

    base html created/ more views added

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to the Pollster Admin Area"
+
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -11,7 +15,10 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [(None, {'fields': ['question_text']}),
-                 ('')]
-
+                 ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
+    inlines = [ChoiceInline]
 # admin.site.register(Question)
 # admin.site.register(Choice)
+
+
+admin.site.register(Question, QuestionAdmin)
