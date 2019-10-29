5452f01f9b21105da3e0803bc9d61579ff5ef544
PremePanther <shpadza@gmail.com>
2019-10-28 20:45:35 -0400

    home/Nav components added & results fixed

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 # Create your views here.
 from .models import Question, Choice
@@ -19,7 +22,7 @@ def detail(request, question_id):
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'polls/results.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 # Get question and display results
 
@@ -27,3 +30,23 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+# Vote for a questoin choice
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You did not select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return a HttpResponseRedirect after dealing with POST data. Stops the posting
+        # of data twice
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
